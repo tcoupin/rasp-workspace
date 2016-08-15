9f4cea15aae8ae6925c0284de036cff25139f1d9
tcoupin <thibault.coupin@gmail.com>
2016-08-15 19:16:39 +0000

    Mongo docker master/slave

---
+++ myImages/docker-mongo/run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+cat > /mongodb/mongodb.conf << EOF
+dbpath=/mongodb/data
+
+
+# Enable journaling, http://www.mongodb.org/display/DOCS/Journaling
+journal=true
+
+# Disable the HTTP interface (Defaults to localhost:28017).
+nohttpinterface = $NOHTTPINTERFACE
+rest = $REST
+
+master = $MASTER
+slave = $SLAVE
+source = $SOURCE
+
+EOF
+
+
+
+/usr/bin/mongod -f /mongodb/mongodb.conf
